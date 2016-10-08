1c2bbaa2dcbe8b8ee4f166cfec1038096f8bfe9c
Cristian Vasile <vasile.cristian@gmail.com>
2016-10-08 21:11:20 +0300

    - added files that must be changed in marmalade sdk to improve the animations.

---
+++ marmalade_changes/8.5/modules/iw2dkitcore/AnimationInstanceContainer.cpp
@@ -0,0 +1,237 @@
+/*
+ * (C) 2001-2012 Marmalade. All Rights Reserved.
+ *
+ * This document is protected by copyright, and contains information
+ * proprietary to Marmalade.
+ *
+ * This file consists of source code released by Marmalade under
+ * the terms of the accompanying End User License Agreement (EULA).
+ * Please do not use this program/source code before you have read the
+ * EULA and have agreed to be bound by its terms.
+ */
+
+#include "AnimationInstanceContainer.h"
+
+#include "AnimationInstance.h"
+#include "Node.h"
+
+M2DKITCORE_NAMESPACE_BEGIN
+
+CAnimationInstanceContainer::CAnimationInstanceContainer()
+    : m_Owner(0)
+    , m_Animations()
+	, m_currentAnimationIndex(-1)
+{
+
+}
+
+CAnimationInstanceContainer::~CAnimationInstanceContainer()
+{
+    for (TAnimationVec::iterator iter = m_Animations.begin(); iter != m_Animations.end(); ++iter)
+    {
+        if (*iter)
+        {
+            delete *iter;
+            *iter = NULL;
+        }
+    }
+
+    m_Animations.clear();
+}
+
+void CAnimationInstanceContainer::SetOwner(CNode* owner)
+{
+    m_Owner = owner;
+}
+
+CAnimationInstance* CAnimationInstanceContainer::AddAnimation(TAnimationSchemaPtr animationSchema, const char* animationName)
+{
+    CAnimationInstance* animationInstance = new CAnimationInstance(animationSchema);
+    if (animationName)
+    {
+        animationInstance->SetName(animationName);
+    }
+    const CAnimationSchemaNode& root = animationSchema->GetRoot();
+
+    if (animationSchema->GetLoop())
+    {
+        animationInstance->SetRepeatCount(0);
+    }
+    else
+    {
+        animationInstance->SetRepeatCount(1);
+    }
+    animationInstance->SetPlaybackDirection(animationSchema->GetPlaybackDirection());
+
+    PopulateAnimationInstance(root, animationInstance);
+
+    animationInstance->AddLabels(animationSchema->GetLabels());
+
+    m_Animations.push_back(animationInstance);
+
+    const std::string label = GetAnimationLabelFromPath(animationSchema->GetName());
+    if (label.empty())
+    {
+        animationInstance->Play();
+    }
+    else
+    {
+        animationInstance->Play(label.c_str());
+    }
+
+    return animationInstance;
+}
+
+CAnimationInstance* CAnimationInstanceContainer::GetAnimation(const char* name)
+{
+    unsigned int nameHash = IwHashString(name);
+    for (TAnimationVec::iterator iter = m_Animations.begin(); iter != m_Animations.end(); ++iter)
+    {
+        if ((*iter)->GetName().GetHash() == nameHash)
+        {
+            return *iter;
+        }
+    }
+
+    return 0;
+}
+
+CAnimationInstance* CAnimationInstanceContainer::GetCurrentAnimation()
+{
+	if (m_currentAnimationIndex >= 0 && m_currentAnimationIndex < m_Animations.size())
+	{
+		return m_Animations[m_currentAnimationIndex];
+	}
+
+	return 0;
+}
+
+CAnimationInstance* CAnimationInstanceContainer::SetCurrentAnimation(const char* name)
+{
+	unsigned int nameHash = IwHashString(name);
+	for (unsigned int i=0; i < m_Animations.size(); ++i)
+	{
+		if (m_Animations[i]->GetName().GetHash() == nameHash)
+		{
+			m_currentAnimationIndex = i;
+			return m_Animations[i];
+		}
+	}
+
+	m_currentAnimationIndex = -1;
+	return 0;
+}
+
+void CAnimationInstanceContainer::Update(float dt)
+{
+	if (m_currentAnimationIndex >= 0 && m_currentAnimationIndex < m_Animations.size())
+	{
+		m_Animations[m_currentAnimationIndex]->Update(dt);
+	}
+	else
+	{
+		for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+		{
+			(*animIter)->Update(dt);
+		}
+	}
+}
+
+void CAnimationInstanceContainer::PopulateAnimationInstance(const CAnimationSchemaNode& schemaNode, CAnimationInstance* animationInstance)
+{
+    PopulateAnimationInstanceRecursive(m_Owner, schemaNode, animationInstance);
+}
+
+void CAnimationInstanceContainer::PopulateAnimationInstanceRecursive(CNode* node, const CAnimationSchemaNode& schemaNode, CAnimationInstance* animationInstance)
+{
+    const CAnimationSchemaNode::TTrackMap& track_map = schemaNode.GetTracks();
+
+    for (CAnimationSchemaNode::TTrackMap::const_iterator iter = track_map.begin(); iter != track_map.end(); ++iter)
+    {
+        const uint32 propertyNameHash = iter->first;
+        node->AddTween(propertyNameHash, iter->second, animationInstance);
+    }
+
+    const CAnimationSchemaNode::TChildVector& m_Children = schemaNode.GetChildren();
+
+    for (CAnimationSchemaNode::TChildVector::const_iterator iter = m_Children.begin(); iter != m_Children.end(); ++iter)
+    {
+        TNodePtr nodeChild = node->FindChildByName(iter->GetName().GetString().c_str());
+        if (nodeChild)
+        {
+            PopulateAnimationInstanceRecursive(nodeChild.get(), *iter, animationInstance);
+        }
+    }
+}
+
+const std::string CAnimationInstanceContainer::GetAnimationLabelFromPath(const std::string& animPath) const
+{
+    std::string label = "";
+    const size_t dotPos = animPath.find_first_of('.');
+    const size_t lastSlashPos = animPath.find_last_of('/');
+    if (dotPos == std::string::npos || lastSlashPos == std::string::npos)
+    {
+        return label;
+    }
+    if (lastSlashPos > dotPos)
+    {
+        label = animPath.substr(lastSlashPos + 1, animPath.length() - lastSlashPos - 1);
+    }
+
+    return label;
+}
+
+void CAnimationInstanceContainer::PlayAll()
+{
+    for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        (*animIter)->Play();
+    }
+}
+
+void CAnimationInstanceContainer::StopAll()
+{
+    for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        (*animIter)->Stop();
+    }
+}
+
+void CAnimationInstanceContainer::PauseAll()
+{
+    for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        (*animIter)->Pause();
+    }
+}
+
+void CAnimationInstanceContainer::ResetAll()
+{
+    for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        (*animIter)->Reset();
+    }
+}
+
+void CAnimationInstanceContainer::RewindAll()
+{
+    for (TAnimationVec::iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        (*animIter)->Rewind();
+    }
+}
+
+bool CAnimationInstanceContainer::HasAnimations() const
+{
+    return !m_Animations.empty();
+}
+
+void CAnimationInstanceContainer::CopyTo(CAnimationInstanceContainer& destination) const
+{
+    for (TAnimationVec::const_iterator animIter = m_Animations.begin(); animIter != m_Animations.end(); ++animIter)
+    {
+        destination.AddAnimation((*animIter)->GetAnimationSchema(), (*animIter)->GetName().GetString().c_str())->Stop();
+    }
+}
+
+M2DKITCORE_NAMESPACE_END
---
+++ marmalade_changes/8.5/modules/iw2dkitcore/AnimationInstanceContainer.h
@@ -0,0 +1,152 @@
+/*
+ * (C) 2001-2012 Marmalade. All Rights Reserved.
+ *
+ * This document is protected by copyright, and contains information
+ * proprietary to Marmalade.
+ *
+ * This file consists of source code released by Marmalade under
+ * the terms of the accompanying End User License Agreement (EULA).
+ * Please do not use this program/source code before you have read the
+ * EULA and have agreed to be bound by its terms.
+ */
+
+#ifndef _ANIMATION_INSTANCE_CONTAINER_H_
+#define _ANIMATION_INSTANCE_CONTAINER_H_
+
+#include "AnimationSchema.h"
+#include "AnimationTrack.h"
+#include "Tween.h"
+
+#include "shared_ptr.hpp"
+#include <vector>
+
+M2DKITCORE_NAMESPACE_BEGIN
+
+class CNode;
+class CAnimationInstance;
+
+/**
+ * @addtogroup m2dkitcoregroup
+ * @{
+ */
+
+/**
+ * @addtogroup animationgroup
+ * @{
+ */
+
+/**
+ * An animation instance container is container for animation instances (CAnimationInstance). The animation
+ * instance container enables a collection of animations to be grouped and updated / controlled using a single
+ * interface. For more information about animations see CAnimationInstance. Nodes each have their own animation
+ * instance container.
+ *
+ */
+class CAnimationInstanceContainer
+{
+public:
+
+    /**
+     * @fn  CAnimationInstanceContainer::CAnimationInstanceContainer();
+     *
+     * Constructs an animation instance container.
+     */
+    CAnimationInstanceContainer();
+    ~CAnimationInstanceContainer();
+
+    /**
+     * Creates an animation instance from an animation schema then adds the animation instance to the animation
+     * instance container.
+     *
+     * @param   animationSchema The animation schema.
+     * @param   animationName   (optional) The name of the animation.
+     *
+     * @return  If non-null, then a pointer to the created animation instance.
+     */
+    CAnimationInstance*             AddAnimation(TAnimationSchemaPtr animationSchema, const char* animationName = 0);
+
+    /**
+     * Gets the named animation instance.
+     *
+     * @param   name    The name of the animation instance.
+     *
+     * @return  If non-null the found animation instance.
+     */
+    CAnimationInstance*             GetAnimation(const char* name);
+
+
+	CAnimationInstance*				GetCurrentAnimation();
+
+	CAnimationInstance*				SetCurrentAnimation(const char* name);
+
+    /**
+     * Updates this animation instance container. The container node will call this to update its animations.
+     *
+     * @param   dt  Delta time in seconds.
+     */
+    void                            Update(float dt);
+
+    /**
+     * Play all animation instances.
+     */
+    void                            PlayAll();
+
+    /**
+     * Stop all animation instances.
+     */
+    void                            StopAll();
+
+    /**
+     * Pause all animation instances.
+     */
+    void                            PauseAll();
+
+    /**
+     * Reset all animation instances.
+     */
+    void                            ResetAll();
+
+    /**
+     * Rewind all animation instances.
+     */
+    void                            RewindAll();
+
+    /**
+     * Query if any of the contained animations are playing.
+     *
+     * @return  true if any animations are playing, false if not.
+     */
+    bool                            HasAnimations() const;
+
+    /**
+     * Copies this animation instance container to the supplied animation instance container.
+     *
+     * @param   destination The destination animation instance container.
+     */
+    void                            CopyTo(CAnimationInstanceContainer& destination) const;
+private:
+
+    typedef std::vector<CAnimationInstance*> TAnimationVec;
+
+    //only friend the node in order to restict the setting of the owner.
+    //This helps us avoid a C4355 warning.
+    friend class CNode;
+
+    void                            SetOwner(CNode* owner);
+    void                            PopulateAnimationInstance(const CAnimationSchemaNode& schemaNode, CAnimationInstance* animationInstance);
+    void                            PopulateAnimationInstanceRecursive(CNode* node, const CAnimationSchemaNode& schemaNode, CAnimationInstance* animationInstance);
+    
+    const std::string               GetAnimationLabelFromPath(const std::string& animPath) const;
+
+    CNode*                          m_Owner;              // Owner of the animation container
+    TAnimationVec                   m_Animations;         // Animation container
+	int								m_currentAnimationIndex;
+};
+
+/** @}*/
+
+/** @}*/
+
+M2DKITCORE_NAMESPACE_END
+
+#endif
